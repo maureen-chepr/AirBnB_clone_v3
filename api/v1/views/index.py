99dbf2a6c18f860f1e23080a3941020535f6ccc3
root <maureenchep95@gmail.com>
2024-02-25 09:20:36 +0300

    task 3

---
+++ api/v1/views/index.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Index section"""
+from models import storage
+from api.v1.views import app_views
+from flask import jsonify
+from models.state import State
+from models.user import User
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
+
+@app_views.route('/stats')
+def stats():
+    """
+        return dict count of data
+    """
+    my_dict = {"states": storage.count(State),
+               "cities": storage.count(City),
+               "places": storage.count(Place),
+               "users": storage.count(User),
+               "amenities": storage.count(Amenity),
+               "reviews": storage.count(Review)}
+    return jsonify(my_dict)
